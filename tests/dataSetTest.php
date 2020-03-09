5ada613d14c5710fc403f3bb63eae3f397d7b643
Mike Garde <mgarde@gmail.com>
2020-03-08 23:58:17 -0400

    PHP 7.4
    Strict datatypes, need to revisit models

--- tests/dataSetTest.php
+++ tests/dataSetTest.php
@@ -8,15 +8,20 @@ class dataSetTest extends TestCase {
 	public function testConstruction()
 	{
 		$countries = new Countries();
-		$dataSet = include 'dataSet.php';
+		$dataSet   = include 'dataSet.php';
 
 		foreach ($dataSet as $term => $desired)
 		{
 			$result = $countries->getCountry($term);
-			$msg    = '"%s" was expecting iso "%s" but got "%s" for "%s"';
-			$msg    = sprintf($msg, $term, $desired, $result['iso2'], $result['name']);
 
-			$this->assertEquals($desired, $result['iso2'], $msg);
+			if (null === $desired) {
+				$this->assertNull($result);
+			} else {
+				$msg = '"%s" was expecting iso "%s" but got "%s" for "%s"';
+				$msg = sprintf($msg, $term, $desired, $result['iso2'], $result['name']);
+	
+				$this->assertEquals($desired, $result['iso2'], $msg);
+			}
 		}
 	}
 }
