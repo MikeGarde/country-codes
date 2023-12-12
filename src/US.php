cb632171027ac7ed7fce823e90e37118aa0ae752
Mike Garde <mgarde@gmail.com>
2023-12-12 12:33:11 -0500

    PHPCS and non-breaking fix

--- src/US.php
+++ src/US.php
@@ -1,4 +1,6 @@
-<?php namespace Countries;
+<?php
+
+namespace Countries;
 
 /*
  * Unlike the countries portion of this package I am not sure I want to correct an address state,
@@ -6,99 +8,88 @@
  * where I think this project is going to head.
  */
 
-class US {
+class US
+{
+    private $strict = true;
+    private $states;
 
-	private $strict = true;
-	private $states;
+    public function __construct($strict = true)
+    {
+        if (!is_bool($strict)) {
+            throw new \Exception('strict must be boolean');
+        }
 
-	public function __construct($strict = true)
-	{
-		if (!is_bool($strict))
-		{
-			throw new \Exception('strict must be boolean');
-		}
+        $this->strict = $strict;
+        $this->states = include '3166_2_US.php';
+    }
 
-		$this->strict = $strict;
-		$this->states = include '3166_2_US.php';
-	}
+    public function getAllStates()
+    {
+        return $this->states;
+    }
 
-	public function getAllStates()
-	{
-		return $this->states;
-	}
+    public function getTypicalStates()
+    {
+        $return = [];
 
-	public function getTypicalStates()
-	{
-		$return = [];
+        foreach ($this->states as $state) {
+            if (in_array($state['type'], ['state', 'district', 'us armed forces'])) {
+                $return[] = $state;
+            }
+        }
 
-		foreach ($this->states as $state)
-		{
-			if (in_array($state['type'], ['state', 'district', 'us armed forces']))
-			{
-				$return[] = $state;
-			}
-		}
+        return $return;
+    }
 
-		return $return;
-	}
+    public function getState($term)
+    {
+        if (!preg_match('/[a-z]/i', $term)) {
+            return null;
+        }
 
-	public function getState($term)
-	{
-		if (!preg_match('/[a-z]/i', $term))
-		{
-			return null;
-		}
+        if ($results = $this->getExactMatch($term)) {
+            return $results;
+        }
 
-		if ($results = $this->getExactMatch($term))
-		{
-			return $results;
-		}
+        if ($this->strict) {
+            return null;
+        }
 
-		if ($this->strict)
-		{
-			return null;
-		}
+        return null;
+    }
 
-		return null;
-	}
+    public function getExactMatch($term)
+    {
+        $term = strtolower($term);
 
-	public function getExactMatch($term)
-	{
-		$term = strtolower($term);
+        foreach ($this->states as $state) {
+            $checkAgainst = [
+                strtolower($state['iso']),
+                strtolower($state['usps']),
+                strtolower($state['name']),
+                strtolower($state['ap']),
+            ];
+            if (in_array($term, $checkAgainst)) {
+                return $state;
+            }
+        }
 
-		foreach ($this->states as $state)
-		{
-			$checkAgainst = [
-				strtolower($state['iso']),
-				strtolower($state['usps']),
-				strtolower($state['name']),
-				strtolower($state['ap']),
-			];
-			if (in_array($term, $checkAgainst))
-			{
-				return $state;
-			}
-		}
+        return false;
+    }
 
-		return false;
-	}
-
-	/**
-	 * Part of the lower 48?
-	 *
-	 * @param $term
-	 *
-	 * @return bool|null
-	 */
-	public function isCONUS($term)
-	{
-		if ($state = $this->getState($term))
-		{
-			return $state['CONUS'];
-		}
-		else
-		{
-			return $state;
-		}
-	}
+    /**
+     * Part of the lower 48?
+     *
+     * @param $term
+     *
+     * @return bool|null
+     */
+    public function isCONUS($term)
+    {
+        if ($state = $this->getState($term)) {
+            return $state['CONUS'];
+        } else {
+            return $state;
+        }
+    }
 }
